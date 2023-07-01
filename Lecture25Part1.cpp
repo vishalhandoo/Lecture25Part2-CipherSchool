d35173336e1e7aa99a71d7f00189bece0a1278d3
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-07-01 13:46:57 +0530

    Add files via upload

---
+++ Lecture25Part1.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student_Data{
+    public:
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+};
+
+int main(){
+
+    Student_Data a,b,c;
+    cout<<"enter the details of the 1st student"<<endl;
+    cin>>a.name;
+    cin>>a.mids;
+    cin>>a.ends;
+    cin>>a.pre_abs;
+    cout<<"enter the details of the 2nd student"<<endl;
+    cin>>b.name;
+    cin>>b.mids;
+    cin>>b.ends;
+    cin>>b.pre_abs;
+    cout<<"enter the details of the 3rd student"<<endl;
+    cin>>c.name;
+    cin>>c.mids;
+    cin>>c.ends;
+    cin>>c.pre_abs;
+
+    cout<<"The details of the students are as follows "<<endl;
+
+    cout<<"Name"<<"\t"<<a.name<<"\t"<<b.name<<"\t"<<c.name<<endl;
+    cout<<"Mids"<<"\t"<<a.mids<<"\t"<<b.mids<<"\t"<<c.mids<<endl;
+    cout<<"Ends"<<"\t"<<a.ends<<"\t"<<b.ends<<"\t"<<c.ends<<endl;
+    cout<<"Attd"<<"\t"<<a.pre_abs<<"\t"<<b.pre_abs<<"\t"<<c.pre_abs;
+
+    return 0;
+}
